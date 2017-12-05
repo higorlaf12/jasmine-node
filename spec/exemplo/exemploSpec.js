de00f7bc595b732ad91061803e88285a1350218b
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-12-05 13:31:05 -0200

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suite de teste de exemplo com NodeJs",function () {
+
+    it("Deve demonstrar uso do Jasmine com o NodeJS",function () {
+        expect(true).toEqual(true);
+    });
+    it("Deve escutar a entrada ",function () {
+        expect(1).toEqual(1);
+    });
+
+
+});
