79702c0dcea8e08ce992d54197dc7a699740a848
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-12-05 14:23:12 -0200

    Arrumando o erro criado

--- spec/exemplo/exemploSpec.js
+++ spec/exemplo/exemploSpec.js
@@ -6,9 +6,4 @@ describe("Suite de teste de exemplo com NodeJs",function () {
     it("Deve escutar a entrada ",function () {
         expect(1).toEqual(1);
     });
-    it("Vai dar erro ",function () {
-       expect(2+2).toEqual(3);
-    });
-
-
 });
